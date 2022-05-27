478e211f5657a542647894ea4c2173f1108322c5
Romina <rominapozzuto@gmail.com>
2022-05-26 21:36:49 -0300

    agrego más usuarios y carritos

--- Ejercicio2Alumno/src/com/curso/pedidos/Ejercicio2.java
+++ Ejercicio2Alumno/src/com/curso/pedidos/Ejercicio2.java
@@ -29,22 +29,38 @@ public class Ejercicio2 {
 
     /**
      * Persistencia de datos
-     * 
-     * @param session 
+     *
+     * @param session
      */
     public static void Create(Session session) {
         Usuario usuario1 = new Usuario("Hora_20", "Perez", 24567890L);
-        Usuario usuario2 = new Usuario("Admin", "Martinez", 32299777L);
+        usuario1.setPassword("1234");
+        usuario1.setNombre("Horacio");
+        usuario1.setFechaDeNacimiento(LocalDate.of(1985, 11, 26));
+        usuario1.setDireccion("Libertador 1234");
+        
+        Usuario usuario2 = new Usuario("Andy_90", "Martinez", 32299777L);
+        usuario2.setPassword("password");
+        usuario2.setNombre("Andrés");
+        usuario2.setFechaDeNacimiento(LocalDate.of(1995, 12, 06));
+        usuario2.setDireccion("Santa Fé 1234");
+        
+        Usuario usuario3 = new Usuario("Eli-3", "Alan", 35291177L);
+        usuario3.setPassword("4356");
+        usuario3.setNombre("Eliana");
+        usuario3.setFechaDeNacimiento(LocalDate.of(1990, 05, 07));
+        usuario3.setDireccion("Juncal 1234");
 
         Producto producto1 = new Producto("M1", "Mesa de madera", 3500.00);
         Producto producto2 = new Producto("MH", "Mesa de hierro", 1060.00);
+        Producto producto3 = new Producto("MS", "Silla", 950.00);
 
         ItemCarrito itemCarrito1 = new ItemCarrito(producto1, 2);
         ItemCarrito itemCarrito2 = new ItemCarrito(producto2, 1);
+        ItemCarrito itemCarrito3 = new ItemCarrito(producto3, 5);
 
         CarritoCompras carritoCompras = new CarritoCompras();
-        carritoCompras.setCantidadMaxItem(1);
-        carritoCompras.setFechaCreacion(LocalDate.of(2022, 05, 26));
+        carritoCompras.setFechaCreacion(LocalDate.now());
         carritoCompras.setUsuario(usuario1);
 
         List<ItemCarrito> items = new ArrayList<>();
@@ -54,13 +70,38 @@ public class Ejercicio2 {
 
         usuario1.setCarritoCompras(carritoCompras);
 
+        CarritoCompras carritoCompras2 = new CarritoCompras();
+        carritoCompras2.setFechaCreacion(LocalDate.now());
+        carritoCompras2.setUsuario(usuario2);
+
+        List<ItemCarrito> items2 = new ArrayList<>();
+        items2.add(itemCarrito3);
+        carritoCompras2.setItemsCarrito(items2);
+
+        usuario2.setCarritoCompras(carritoCompras2);
+
+        CarritoCompras carritoCompras3 = new CarritoCompras();
+        carritoCompras3.setFechaCreacion(LocalDate.now());
+        carritoCompras3.setUsuario(usuario3);
+
+        List<ItemCarrito> items3 = new ArrayList<>();
+        items3.add(itemCarrito3);
+        items3.add(itemCarrito2);
+        carritoCompras3.setItemsCarrito(items3);
+
+        usuario3.setCarritoCompras(carritoCompras3);
+
         session.save(usuario1);
         session.save(usuario2);
         session.save(producto1);
         session.save(producto2);
+        session.save(producto3);
         session.save(carritoCompras);
+        session.save(carritoCompras2);
+        session.save(carritoCompras3);
         session.save(itemCarrito1);
         session.save(itemCarrito2);
+        session.save(itemCarrito3);
     }
 
     /**
